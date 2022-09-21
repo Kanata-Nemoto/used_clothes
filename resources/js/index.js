19457d16044625526e5b2371f14222da64a8fe14
Kanata-Nemoto <k.nemoto.0327@gmail.com>
2022-09-21 18:32:45 +0900

    [test]index.js

--- resources/js/index.js
+++ resources/js/index.js
@@ -11,7 +11,7 @@ window.initAutocomplete = function () {
  
  // マーカー毎の処理
   for (var i = 0; i < markerData.length; i++) {
-    var markerLatLng = new google.maps.LatLng({lat: markerData[i]['lat'], lng: markerData[i]['lng']}); // 緯度経度のデータ作成
+    var markerLatLng = new google.maps.LatLng({lat: 35.66372992897464, lng: 139.66810707930617}); // 緯度経度のデータ作成
     marker[i] = new google.maps.Marker({ // マーカーの追加
       position: markerLatLng, // マーカーを立てる位置を指定
       map: map // マーカーを立てる地図を指定
